fe4dfc7f456b82e07f14adc91ed913e12dee7d0c
Kauan Santos <kauansantosdepontes@gmail.com>
2024-06-19 14:11:38 -0300

    chore: removed favicon and background tailwind unused

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,11 +11,6 @@ const config: Config = {
       colors: {
         brown: '#4F372F',
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
